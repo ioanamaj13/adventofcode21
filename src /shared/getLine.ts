1e482e58c9ab0fd3b1df538b59ea4b7c1b197318
Ioana Sauleac <ioana.sauleac@prudential.co.uk>
2021-12-06 14:01:57 +0000

    updates and cleanup

--- src /shared/getLine.ts
+++ src /shared/getLine.ts
@@ -1,22 +1,9 @@
 const fs = require('fs'); 
-const readline = require('readline');
 
-export async function getLine(path: string, lineNo: number): Promise<string[]> {
-    const input = [];
-    const fileStream = fs.createReadStream(path);
+export function getInput(fileName: string): string {
+  return fs.readFileSync(`../inputs/${fileName}.txt`, 'utf8').replace(/\r/g, '');
+}
 
-    console.log(fileStream); 
-  
-    const rl = readline.createInterface({
-      input: fileStream,
-      crlfDelay: Infinity,
-    });
-  
-    for await (const line of rl) {
-      input.push(line);
-    }
-    
-    return input;
-  }
-
-getLine('../inputs/day4.txt', 0).then(res => console.log(res)); 
+export function getLine(fileName: string, separator = '\n'): string[] {
+  return getInput(fileName).split(separator);
+}
